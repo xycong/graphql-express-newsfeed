ee7d405b872f61e97239786867cd7977ef8e840e
Victor Cong <xycong@uwaterloo.ca>
2018-01-03 16:40:11 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+const schema = require('./schema');
+
+const connectMongo = require('./mongo-connector');
+
+const start = async () => {
+  const mongo = await connectMongo();
+  var app = express();
+  app.use('/graphql', bodyParser.json(), graphqlExpress({
+    context: { mongo },
+    schema
+  }));
+  app.use('/graphiql', graphiqlExpress({
+    endpointURL: '/graphql',
+  }));
+
+
+  const PORT = 3000;
+  app.listen(PORT, () => {
+    console.log(`Hackernews GraphQL server running on port ${PORT}.`)
+  });
+}
+
+start();
---
+++ src/schema/resolvers.js
@@ -0,0 +1,18 @@
+module.exports = {
+  Query: {
+    allLinks: async (root, data, { mongo: { Links } }) => {
+      return await Links.find({}).toArray();
+    },
+  },
+
+  Mutation: {
+    createLink: async (root, data, { mongo: { Links } }) => {
+      const response = await Links.insert(data);
+      return Object.assign({ id: response.insertedIds[0] }, data);
+    },
+  },
+
+  Link: {
+    id: root => root._id || root.id,
+  },
+};
