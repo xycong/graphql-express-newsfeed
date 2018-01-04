235fd2734895621a8f761d101c036fa6f507d654
Victor Cong <xycong@uwaterloo.ca>
2018-01-04 14:19:03 -0500

    Added voting to links

--- src/schema/index.js
+++ src/schema/index.js
@@ -7,6 +7,7 @@ const typeDefs = `
     url: String!
     description: String!
     postedBy: User
+    votes: [Vote!]!
   }
 
   type Query {
@@ -15,6 +16,7 @@ const typeDefs = `
 
   type Mutation {
     createLink(url: String!, description: String!): Link
+    createVote(linkId: ID!): Vote
     createUser(name: String!, authProvider: AuthProviderSignupData!): User
     signinUser(email: AUTH_PROVIDER_EMAIL): SigninPayload!
   }
@@ -28,6 +30,14 @@ const typeDefs = `
     id: ID!
     name: String!
     email: String
+    password: String
+    votes: [Vote!]!
+  }
+
+  type Vote {
+    id: ID!
+    user: User!
+    link: Link!
   }
 
   input AuthProviderSignupData {
--- src/schema/resolvers.js
+++ src/schema/resolvers.js
@@ -1,3 +1,5 @@
+const { ObjectID } = require('mongodb')
+
 module.exports = {
   Query: {
     allLinks: async (root, data, { mongo: { Links } }) => {
@@ -26,6 +28,14 @@ module.exports = {
         return { token: `token-${user.email}`, user };
       }
     },
+    createVote: async (root, data, { mongo: { Votes }, user }) => {
+      const newVote = {
+        userId: user && user._id,
+        linkId: new ObjectID(data.linkId),
+      };
+      const response = await Votes.insert(newVote);
+      return Object.assign({ id: response.insertedIds[0] }, newVote);
+    },
   },
 
   Link: {
@@ -34,9 +44,29 @@ module.exports = {
     postedBy: async ({ postedById }, data, { mongo: { Users } }) => {
       return await Users.findOne({ _id: postedById });
     },
+
+    votes: async ({_id}, data, {mongo: {Votes}}) => {
+      return await Votes.find({linkId: _id}).toArray();
+    }
   },
 
   User: {
     id: root => root._id || root.id,
-  }
+
+    votes: async ({_id}, data, {mongo: {Votes}}) => {
+      return await Votes.find({userId: _id}).toArray();
+    }
+  },
+
+  Vote: {
+    id: root => root._id || root.id,
+
+    user: async ({ userId }, data, { mongo: { Users } }) => {
+      return await Users.findOne({ _id: userId });
+    },
+
+    link: async ({ linkId }, data, { mongo: { Links } }) => {
+      return await Links.findOne({ _id: linkId });
+    },
+  },
 };
